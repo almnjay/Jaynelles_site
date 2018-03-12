114f07a3774ebe708e08eab726eec5f44b0a92fc
almnjay <37200315+almnjay@users.noreply.github.com>
2018-03-12 00:43:51 -0400

    Removed "Email" section

--- about.php
+++ about.php
@@ -27,7 +27,6 @@
 </nav>
 
 <h2>Full Name:</h2><p>Jaynelle A. Aleman</p>
-	<h2>Email:</h2><p>jaynellealeman@gmail.com</p>
 	<img src="Profile.png" alt="Profile">
 <h2>About Me:</h2>
 	<p> Hello! I am Jaynelle but I usually go by "Jay". I am a student at Valenica college currently majoring in Motion Graphics. I have been working with photoshop since I was 14 years old, mostly drawing art or editing my personal photgraphy. I love working with different mediums of art and once I had discovered graphic arts, I fell inlove with the endless ways to create whether it be hand drawn or painting or creating collages and much more. Although, recently I have taken a liking to web design because it is one big creative puzzle. I am excited to learn more and expand my artistic mediums. 
