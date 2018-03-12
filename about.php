c43f2762858b248e1f14353365140ef72035c6bb
almnjay <37200315+almnjay@users.noreply.github.com>
2018-03-12 01:54:43 -0400

    Added Content

--- about.php
+++ about.php
@@ -27,6 +27,8 @@
 </nav>
 
 <h2>Full Name:</h2><p>Jaynelle A. Aleman</p>
+<h1>Birthdate:</h1><p>December 20th, 1997</p>
+<h1>MBTI Personality:</h1><p>INFP</p>
 	<img src="Profile.png" alt="Profile">
 <h2>About Me:</h2>
 	<p> Hello! I am Jaynelle but I usually go by "Jay". I am a student at Valenica college currently majoring in Motion Graphics. I have been working with photoshop since I was 14 years old, mostly drawing art or editing my personal photgraphy. I love working with different mediums of art and once I had discovered graphic arts, I fell inlove with the endless ways to create whether it be hand drawn or painting or creating collages and much more. Although, recently I have taken a liking to web design because it is one big creative puzzle. I am excited to learn more and expand my artistic mediums. 
