ae9da566134e1ec9a5b32860e19846d425104e59
almnjay <jaynellealeman@gmail.com>
2018-03-14 12:35:26 -0400

    fixed to be resume ready

--- about.php
+++ about.php
@@ -29,7 +29,7 @@
 <h1>Birthdate:</h1><p>December 20th, 1997</p>
 	<img src="Profile.png" alt="Profile">
 <h1>About Me:</h1>
-	<p> Hello! I am Jaynelle but I usually go by "Jay". I am a student at Valenica college currently majoring in Motion Graphics. I have been working with photoshop since I was 14 years old, mostly drawing art or editing my personal photgraphy. I love working with different mediums of art and once I had discovered graphic arts, I fell inlove with the endless ways to create whether it be hand drawn or painting or creating collages and much more. Although, recently I have taken a liking to web design because it is one big creative puzzle. I am excited to learn more and expand my artistic mediums. 
+	<p> Hello! I am Jaynelle but I usually go by "Jay". I am a student at Valenica college currently majoring in Motion Graphics. I have been working with photoshop since I was 14 years old, mostly drawing art or editing my personal photgraphy. I love working with different mediums of art and once I had discovered graphic arts, I fell in love with the endless ways to create whether it be hand drawn or painting or creating collages and much more. Although, recently I have taken a liking to web design because it is one big creative puzzle. I am excited to learn more and expand my artistic mediums. 
 
 </div>
 <div class="push">
